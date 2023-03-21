003ea1775be17d797502ff2338b1c92432c8f485
Emmanuel <emmanuel.arana.gutierrez@gmail.com>
2023-03-20 23:30:05 -0600

    cambios

--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -3,15 +3,15 @@ import React, { Fragment } from "react";
 const Footer = () => {
     return (
         <Fragment>
-            <footer class="bg-light py-3">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6 text-center text-md-start">
+            <footer className="bg-light py-3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 text-center text-md-start">
                             <p>&copy; 2023 My App</p>
                         </div>
-                        <div class="col-md-6 text-center text-md-end">
+                        <div className="col-md-6 text-center text-md-end">
                             <a href="#">Privacy Policy</a>
-                            <span class="mx-2">|</span>
+                            <span className="mx-2">|</span>
                             <a href="#">Terms of Use</a>
                         </div>
                     </div>
--- src/components/template/Header.jsx
+++ src/components/template/Header.jsx
@@ -5,22 +5,22 @@ const Header = () => {
     return (
         <Fragment>
             <header>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light | ">
-                    <div class="container">
-                        <a class="navbar-brand" href="#">My App</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light | ">
+                    <div className="container">
+                        <a className="navbar-brand" href="#">My App</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav ms-auto">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Home</a>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav ms-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Home</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">About</a>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">About</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Contact</a>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Contact</a>
                                 </li>
                             </ul>
                         </div>
